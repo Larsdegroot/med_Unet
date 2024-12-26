272752c136852e577d06ba1c1cf71c959e200b3c
Lars de Groot <larsdgrt@gmail.com>
2024-12-26 13:51:43 +0100

    started implementing Brats

--- data_loading.py
+++ data_loading.py
@@ -1,3 +1,4 @@
+import SimpleITK as sitk
 from sklearn.model_selection import train_test_split
 from pathlib import Path
 from os.path import abspath
@@ -5,6 +6,7 @@ from torch.utils.data import DataLoader
 from lightning import LightningDataModule
 from monai.data import CacheDataset
 from monai.transforms import Compose, LoadImaged, EnsureChannelFirstd, ResizeD, NormalizeIntensityd
+from lightning.pytorch.utilities.exceptions import MisconfigurationException
 
 
 ### TODO:
@@ -12,13 +14,20 @@ from monai.transforms import Compose, LoadImaged, EnsureChannelFirstd, ResizeD, 
 # Eva - Expand on transformations
 # Someone - Implement a way to setup the dataset for 2D data
 
-class WMHDataModule(LightningDataModule):
+class MRIDataModule(LightningDataModule):
     """
     PyTorch Lightning DataModule for the WMH dataset.
     Handles data collection, transformations, and DataLoader creation.
     """
 
-    def __init__(self, data_dir: str, batch_size: int = 8, num_workers: int = 8):
+    def __init__(
+        self,
+        dataset: str = "WMH",
+        data_dir_wmh: str = "data/wmh",
+        data_dir_brats: str = "data/BraTS"
+        batch_size: int = 8,
+        num_workers: int = 8
+    ):
         """
         Initialize the data module.
 
@@ -32,6 +41,14 @@ class WMHDataModule(LightningDataModule):
             Number of subprocesses to use during data loading, decrease for slightly less memory usage.
         """
         super().__init__()
+        
+        if dataset.lower() == "wmh":
+            data_dir = data_dir_wmh
+        elif dataset.lower() == "brats":
+            data_dir = data_dir_brats
+        else:
+            raise MisconfigurationException(f'"{dataset}" is not a supported dataset, use either "WMH" or "brats"')
+            
         self.data_dir = Path(data_dir)
         self.batch_size = batch_size
         self.num_workers = num_workers
@@ -79,6 +96,34 @@ class WMHDataModule(LightningDataModule):
                     print(f"Missing files in {subject_dir}")
         return samples
 
+    def collect_samples_brats(self, root: Path) -> list[dict]:
+        """
+        Collect all the valid sample paths from the wmh dataset.
+
+        Parameters
+        ----------
+        root : Path
+            Root path of the dataset.
+
+        Returns
+        -------
+        list[dict]
+            List of dictionaries with paths to FLAIR, T1, and WMH files.
+        """
+        samples = []
+        for subject_dir in root.iterdir():
+            if subject_dir.is_dir():
+                flair_path = subject_dir / "FLAIR.nii.gz"
+                t1_path = subject_dir / "T1.nii.gz"
+                wmh_path = subject_dir / "wmh.nii.gz"
+
+                # Ensure all necessary files exist
+                if flair_path.exists() and t1_path.exists() and wmh_path.exists():
+                    samples.append({"flair": flair_path, "t1": t1_path, "WMH": wmh_path})
+                else:
+                    print(f"Missing files in {subject_dir}")
+        return samples
+
     def setup(self, stage: str = None):
         """
         Load and split the data into train, val, and test sets.
@@ -88,13 +133,14 @@ class WMHDataModule(LightningDataModule):
         stage : str
             Stage of training: 'fit', 'test', etc.
         """
-        # Collect samples from the dataset
+        # Collect sample paths from the dataset
         all_samples = []
         for subject_path in self.data_dir.iterdir():
             if subject_path.is_dir():
                 all_samples.extend(self.collect_samples_wmh(subject_path))
 
         # Split into train, validation, and test sets
+        if self.
         train_samples, temp_samples = train_test_split(all_samples, train_size=0.8, random_state=42, shuffle=True)
         val_samples, test_samples = train_test_split(temp_samples, test_size=0.5, random_state=42, shuffle=True)
 
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from data_loading import WMHDataModule
+from data_loading import MRIDataModule
 from model import LitUNet
 from lightning.pytorch.cli import LightningCLI
+from lightning.pytorch.utilities.exceptions import MisconfigurationException
+
+# class MyLightningCLI(LightningCLI):
+#     def add_arguments_to_parser(self, parser):
+#         parser.add_argument("--dataset", default="WMH")
+
+#     def before_instantiate_classes(self):
+#         if self.config["dataset"] == "WMH":
+#             self.datamodule_class = WMHDataModule
+#         elif self.config["dataset"].lower() == "brats":
+#             self.datamodule_class = BratsDataModule
+#         else:
+#             raise MisconfigurationException(f'"{self.config["dataset"]}" is not a supported dataset, use either "WMH" or "brats" ')
 
 def cli_main():
-    cli = LightningCLI(LitUNet, WMHDataModule)
+    cli = MyLightningCLI(LitUNet, MRIDataModule)
 
 
 if __name__ == "__main__":
