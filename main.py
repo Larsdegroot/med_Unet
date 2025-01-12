a38ffc00dc71ad912c5e0cc5e5914312965a51b0
samar-alt <71869487+samar-alt@users.noreply.github.com>
2025-01-12 11:38:50 +0100

    adjusted code to work with rclone, didn't test yet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from data_loading import MRIDataModule
 from model import LitUNet
 from lightning.pytorch.cli import LightningCLI
+from pytorch_lightning.loggers import TensorBoardLogger
 from lightning.pytorch.utilities.exceptions import MisconfigurationException
 
 # class MyLightningCLI(LightningCLI):
@@ -16,8 +17,18 @@ from lightning.pytorch.utilities.exceptions import MisconfigurationException
 #             raise MisconfigurationException(f'"{self.config["dataset"]}" is not a supported dataset, use either "WMH" or "brats" ')
 
 def cli_main():
-    cli = LightningCLI(LitUNet, MRIDataModule)
+    cli = LightningCLI(LitUNet, MRIDataModule, save_config_overwrite=True)
 
+    # config logger using config file
+    log_config = cli.config['trainer']['logger']
+    logger = TensorBoardLogger(save_dir=log_config['save_dir'], name=log_config['name'])
+    cli.trainer.logger = logger
+
+    # config data folders using config file
+    data_path_wmh = cli.config['data']['data_dir_wmh']
+    cli.datamodule.data_dir_wmh = data_path_wmh
+    data_path_brats = cli.config['data']['data_dir_brats']
+    cli.datamodule.data_dir_brats = data_path_brats
 
 if __name__ == "__main__":
     cli_main()
